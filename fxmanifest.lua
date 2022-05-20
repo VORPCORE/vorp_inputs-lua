34bcc8ded2d1ee7bbb39b7dd7bd0c92856f7b028
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-20 22:22:09 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,8 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 ui_page 'html/index.html'
 
+author 'Emollit'
+
 client_scripts {
   'client/models/*.lua',
   'client/services/*.lua',
@@ -12,3 +14,8 @@ client_scripts {
 files {
   'html/**/*'
 }
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_inputs-lua'
