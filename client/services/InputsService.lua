80856695197f91a8301b142e6bfafcd8a98b6bc1
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-31 00:07:29 +0100

    fix for placeholder not showing on charecter creation

--- client/services/InputsService.lua
+++ client/services/InputsService.lua
@@ -1,25 +1,25 @@
 InputsService = {}
 local text = nil
 
-InputsService.CloseInput = function ()
+InputsService.CloseInput = function()
     SetNuiFocus(false, false)
-    SendNUIMessage(NUIEvent:New({style= "none"}))
+    SendNUIMessage(NUIEvent:New({ style = "none" }))
 end
 
 ---@param result table
-InputsService.SetSubmit = function (result)
+InputsService.SetSubmit = function(result)
     text = result.stringtext
 end
 
 ---@param result table
-InputsService.SetClose = function (result)
+InputsService.SetClose = function(result)
     text = result.stringtext
 end
 
 ---@param title string
 ---@param placeHolder string
 ---@param cb function
-InputsService.GetInputs = function (title, placeHolder, cb)
+InputsService.GetInputs = function(title, placeHolder, cb)
     InputsService.WaitForInputs(title, placeHolder, cb)
 end
 
@@ -27,13 +27,13 @@ end
 ---@param placeHolder string
 ---@param inputType string
 ---@param cb function
-InputsService.GetInputsWithInputType = function (title, placeHolder, inputType, cb)
+InputsService.GetInputsWithInputType = function(title, placeHolder, inputType, cb)
     InputsService.WaitForInputs(title, placeHolder, cb, inputType)
 end
 
 ---@param inputConfig string
 ---@param cb function
-InputsService.OnAdvancedInput = function (inputConfig, cb)
+InputsService.OnAdvancedInput = function(inputConfig, cb)
     SetNuiFocus(true, true)
     SendNUIMessage(json.decode(inputConfig))
 
@@ -53,15 +53,15 @@ end
 ---@param placeHolder string
 ---@param cb function
 ---@param inputType string
-InputsService.WaitForInputs = function (button, placeHolder, cb, inputType)
-    inputType = inputType or "textarea"
+InputsService.WaitForInputs = function(button, placeHolder, cb, inputType)
+    inputType = inputType or "input" or "textarea"
 
     SetNuiFocus(true, true)
     SendNUIMessage(NUIEvent:New({
-        style= "block",
-        button= button,
-        placeholder= placeHolder,
-        inputType= inputType,
+        style = "block",
+        button = button,
+        placeholder = placeHolder,
+        inputType = inputType,
     }))
 
     while text == nil do
